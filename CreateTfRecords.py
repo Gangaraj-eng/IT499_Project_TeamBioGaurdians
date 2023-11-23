560c8e7236da908441ac80ccd6bc07ec624bf993
RAJU <202001107@daiict.ac.in>
2023-11-24 03:47:29 +0530

    Updated Dynamic architecture

--- CreateTfRecords.py
+++ CreateTfRecords.py
@@ -1,28 +1,114 @@
-from random import shuffle
+# CreateTfRecords
+# This file contains the functions to create tensorflow records
+# from the input data for static and dynamic architectures
+
+# importing required libraries
 import cv2
 import tensorflow as tf
 import sys
 import pandas as pd
 import os
-from videoPreProcessing import frames_from_video_file
 import numpy as np
-from audioPreprocessing import extract_audio_feature
+from AudioHelpers import extractAudioFeatures
+from VideoHelpers import frames_from_video_files
 
+# function to serailze and convert a ndarray to a bytelist tensorflow feature
 def _float_array_feature(array):
     tensor = tf.convert_to_tensor(array)
     value =  tf.io.serialize_tensor(tensor).numpy()
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+# function to convert an integer to tensorflow feature
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+  
+# required output image dimensions
+ImageDimensions = (224,224)
 
-
-# required output dimensions for the image
-img_dimensions = (224,224)
-
+# paths to the folders contains audio video and image data
+# change this to the folder containing the data files
 rootAudioPath = r'D:\BiometricProject\Mini_Data\Audio\Dev'
 rootVideoPath = r'D:\BiometricProject\Mini_Data\Video\Dev'
 rootImgPath = r'D:\BiometricProject\Mini_Data\Images\Dev'
+
+def load_imag(imgPath):
+     # read the image and resize into a fixed size
+     
+     img = cv2.imread(os.path.join(rootImgPath, imgPath))
+     
+     if img is None:
+       return None
+     
+     # perform resizing 
+     img = cv2.resize(img, img_dimensions)
+     
+     # rescaling to normalize
+     img = img/255.0
+     return img
+
+def createDataRecord_static1(outfileName, img_addrs, audio_addrs, labels):
+    
+    writer = tf.io.TFRecordWriter(outfileName)
+    
+    for i in range(len(img_addrs)):
+      
+      if i%1000 == 0:
+        print('Created {}/{} records'.format(i, len(img_addrs)))
+        sys.stdout.flush()
+        
+      img = load_imag(img_addrs[i])
+      
+      # load audio file image
+      audio = extract_audio_feature(os.path.join(rootAudioPath,audio_addrs[i]), 'mfcc', n_mfcc=20)
+      audio = np.array(audio, dtype=np.float64)
+      label = labels[i]
+      
+      if img is None:
+        continue
+      feature={
+        'image_raw': _float_array_feature(img),
+        'audio_raw': _float_array_feature(audio),
+        'label': _int64_feature(label)
+      }
+      
+      example = tf.train.Example(features = tf.train.Features(feature=feature))
+      writer.write(example.SerializeToString())
+      
+    writer.close()
+    sys.stdout.flush()
+    
+
+def createDataRecord_static2(outfileName, img1_addrs, img2_addrs, audio_addrs, labels):
+    writer = tf.io.TFRecordWriter(outfileName)
+    
+    for i in range(len(audio_addrs)):
+      
+      if i%1000 == 0:
+        print('Created {}/{} records'.format(i, len(audio_addrs)))
+        sys.stdout.flush()
+        
+      img1 = load_imag(img1_addrs[i])
+      img2 = load_imag(img2_addrs[i])
+      
+      # load audio file image
+      audio = extract_audio_feature(os.path.join(rootAudioPath,audio_addrs[i]), 'mfcc', n_mfcc=20)
+      audio = np.array(audio, dtype=np.float64)
+      label = labels[i]
+      
+      if img1 is None or img2 is None:
+        continue
+      feature={
+        'image1_raw': _float_array_feature(img1),
+        'image2_raw': _float_array_feature(img2),
+        'audio_raw': _float_array_feature(audio),
+        'label': _int64_feature(label)
+      }
+      
+      example = tf.train.Example(features = tf.train.Features(feature=feature))
+      writer.write(example.SerializeToString())
+      
+    writer.close()
+    sys.stdout.flush()
 
 # creates Tf Records with the inputs as follows
 # x = (face, video, audio) 
@@ -107,18 +193,3 @@ def create_Data_Record_Dynamic2(outfileName, img1_addrs, img2_addrs, video_addrs
 
     writer.close() 
     sys.stdout.flush()
-
-def load_imag(imgPath):
-     # read the image and resize into a fixed size
-     
-     img = cv2.imread(os.path.join(rootImgPath, imgPath))
-     
-     if img is None:
-       return None
-     
-     # perform resizing 
-     img = cv2.resize(img, img_dimensions)
-     
-     # rescaling to normalize
-     img = img/255.0
-     return img
--- VideoHelpers.py
+++ VideoHelpers.py
@@ -1,45 +1,55 @@
+# VideoHelpers.py
+# this file contains required functions to 
+# extract frames from video files
+
+# importing required packages
 import tensorflow as tf
-import cv2
+import cv2 
 import random
 import numpy as np
 
+FrameDimensions = (224,224)
+NFrames = 10
+FrameStep = 15
 
-# function that resize a frame into desired output size
-def format_frames(frame, output_size = (224,224)):
+# function that resizes a frame into a fixed dimensions
+# and converts into a tensor
+def format_frame(frame, output_size = FrameDimensions):
   frame = tf.image.convert_image_dtype(frame, tf.float32)
   # resize with padding inorder to obtain the desired shape
   frame = tf.image.resize_with_pad(frame, *output_size)
+  frame = frame/255.0
   return frame
 
 # function to extract the frames form a given video file path
 # n_frames - number of output frames to be extracted
 # output_size - dimensions of each frame
 # frame_stpe - gap between two collected frames
-def frames_from_video_file(video_path, n_frames, output_size = (224,224), frame_step = 15):
-    result = []
-    src = cv2.VideoCapture(str(video_path))  
+def frames_from_video_files(video_path, n_frames, output_size = FrameDimensions, frame_step = 15):
+  result = []
+  src = cv2.VideoCapture(str(video_path))
+  
+  video_length = src.get(cv2.CAP_PROP_FRAME_COUNT)
+  
+  need_length = 1 + (n_frames - 1) * frame_step
 
-    video_length = src.get(cv2.CAP_PROP_FRAME_COUNT)
-
-    need_length = 1 + (n_frames - 1) * frame_step
-
-    if need_length > video_length:
-      start = 0
-    else:
-      max_start = video_length - need_length
-      start = random.randint(0, max_start + 1)
+  if need_length > video_length:
+    start = 0
+  else:
+    max_start = video_length - need_length
+    start = random.randint(0, max_start + 1)
 
     src.set(cv2.CAP_PROP_POS_FRAMES, start)
     
     # ret is a boolean indicating whether read was successful, frame is the image itself
     ret, frame = src.read()
-    result.append(format_frames(frame, output_size))
+    result.append(format_frame(frame, output_size))
 
     for _ in range(n_frames - 1):
       for _ in range(frame_step):
         ret, frame = src.read()
       if ret:
-        frame = format_frames(frame, output_size)
+        frame = format_frame(frame, output_size)
         result.append(frame)
       else:
         result.append(np.zeros_like(result[0]))
